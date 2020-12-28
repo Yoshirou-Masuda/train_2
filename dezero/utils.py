688648a168f6a0c4f2ce9313fa69eaaf6c1bc9e5
Yoshirou-Masuda <76555049+Yoshirou-Masuda@users.noreply.github.com>
2020-12-28 10:15:06 +0900

    Update utils.py

--- dezero/utils.py
+++ dezero/utils.py
@@ -4,6 +4,9 @@ import numpy as np
 from dezero import as_variable
 from dezero import Variable
 
+# =============================================================================
+# Visualize for computational graph
+# =============================================================================
 def _dot_var(v, verbose=False):
     dot_var = '{} [label="{}", color=orange, style=filled]\n'
 
@@ -12,22 +15,38 @@ def _dot_var(v, verbose=False):
         if v.name is not None:
             name += ': '
         name += str(v.shape) + ' ' + str(v.dtype)
+
     return dot_var.format(id(v), name)
 
-#DOT言語に変換
-def _dot_func(f):
-    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
-    txt = dot_func.format(id(f), f.__class__.__name__)
 
+def _dot_func(f):
+    # for function
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    ret = dot_func.format(id(f), f.__class__.__name__)
+
+    # for edge
     dot_edge = '{} -> {}\n'
     for x in f.inputs:
-        txt += dot_edge.format(id(x), id(f))
-    for y in f.outputs:
-        txt += dot_edge.format(id(f), id(y()))#y is weakref
-    return txt
+        ret += dot_edge.format(id(x), id(f))
+    for y in f.outputs:  # y is weakref
+        ret += dot_edge.format(id(f), id(y()))
+    return ret
 
-#計算グラフの可視化
+
 def get_dot_graph(output, verbose=True):
+    """Generates a graphviz DOT text of a computational graph.
+    Build a graph of functions and variables backward-reachable from the
+    output. To visualize a graphviz DOT text, you need the dot binary from the
+    graphviz package (www.graphviz.org).
+    Args:
+        output (dezero.Variable): Output variable from which the graph is
+            constructed.
+        verbose (bool): If True the dot graph contains additional information
+            such as shapes and dtypes.
+    Returns:
+        str: A graphviz DOT text consisting of nodes and edges that are
+            backward-reachable from the output
+    """
     txt = ''
     funcs = []
     seen_set = set()
@@ -37,34 +56,33 @@ def get_dot_graph(output, verbose=True):
             funcs.append(f)
             # funcs.sort(key=lambda x: x.generation)
             seen_set.add(f)
-        
-        add_func(output.creator)
-        txt += _dot_var(output, verpose)
 
-        while funcs:
-            func = funcs.pop()
-            txt += _dot_func(func)
-            for x in func.inputs:
-                txt += _dot_var(x, verbose)
+    add_func(output.creator)
+    txt += _dot_var(output, verbose)
 
-                if x.creator is not None:
-                    add_func(x.creator)
-        return 'digraph g {\n' + txt + '}' 
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x, verbose)
 
-#画像かまでを含めた変換
+            if x.creator is not None:
+                add_func(x.creator)
+
+    return 'digraph g {\n' + txt + '}'
+
+
 def plot_dot_graph(output, verbose=True, to_file='graph.png'):
     dot_graph = get_dot_graph(output, verbose)
 
-    # dotデータをファイルに保存
     tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
-    if not os.path.exists(tmp_dir): #dezeroディレクトリがなかったら作成
+    if not os.path.exists(tmp_dir):
         os.mkdir(tmp_dir)
     graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
 
     with open(graph_path, 'w') as f:
         f.write(dot_graph)
 
-    # dotコマンドを呼ぶ
-    extension = os.path.splitext(to_file)[1][1:] #拡張子png,pdf etc
-    cmd = 'dot {} -T {} o {}'.format(graph_path, extension, to_file)
-    subprocess.run(cmd, shell=True)   
+    extension = os.path.splitext(to_file)[1][1:]  # Extension(e.g. png, pdf)
+    cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
+    subprocess.run(cmd, shell=True)
