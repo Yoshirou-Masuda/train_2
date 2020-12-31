58432a5432209e164868a69dc17b1f4a0ae6cca5
Yoshirou-Masuda <76555049+Yoshirou-Masuda@users.noreply.github.com>
2020-12-31 16:44:41 +0900

    Update core.py

--- dezero/core.py
+++ dezero/core.py
@@ -212,7 +212,12 @@ class Mul(Function):
 
     def backward(self, gy):
         x0, x1 = self.inputs
-        return gy * x1, gy * x0
+        gx0 = gy * x1
+        gx1 = gy * x0
+        if x0.shape != x.shape:
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
+        return gx0, gx1    
 
 def mul(x0, x1):
     x1= as_array(x1)
