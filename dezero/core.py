b2580388083508ba9dcc2d1b3c744b0e56ee06c2
Yoshirou-Masuda <76555049+Yoshirou-Masuda@users.noreply.github.com>
2021-01-01 13:35:06 +0900

    Update core.py

--- dezero/core.py
+++ dezero/core.py
@@ -26,12 +26,17 @@ def no_grad():
 # =============================================================================
 # Variable / Function
 # =============================================================================
-
+try:
+    import cupy
+    array_types = (np.ndarray, cupy.ndarray)
+except ImportError:
+    array_types = (np.ndarray)
+    
 class Variable:
     __array_priority__ = 200
     def __init__(self, data, name=None):
         if data is not None:
-            if not isinstance(data, np.ndarray):
+            if not isinstance(data, array_types):
                 raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
@@ -39,64 +44,6 @@ class Variable:
         self.grad = None
         self.creator = None
         self.generation = 0
-
-    def set_creator(self, func):
-        self.creator = func
-        self.generation = func.generation + 1
-
-    def cleargrad(self):
-        self.grad = None
-
-    def backward(self,retain_grad=False, create_graph=False):
-        if self.grad is None:
-            #self.grad = np.ones_like(self.data)
-            self.grad = Variable(np.ones_like(self.data))
-
-        funcs = []
-        seen_set = set()
-
-        def add_func(f):
-            if f not in seen_set:
-                funcs.append(f)
-                seen_set.add(f)
-                funcs.sort(key=lambda x: x.generation)
-
-        add_func(self.creator)
-
-        while funcs:
-            f = funcs.pop()
-            gys = [output().grad for output in f.outputs]
-            
-            with using_config('enable_backprop', create_graph):
-                gxs = f.backward(*gys) #メインのbackward
-            if not isinstance(gxs, tuple):
-                gxs = (gxs,)
-
-            for x, gx in zip(f.inputs, gxs):
-                if x.grad is None:
-                    x.grad = gx
-                else:
-                    x.grad = x.grad + gx #この計算も対象
-
-                if x.creator is not None:
-                    add_func(x.creator)
-            
-            if not retain_grad:
-                for y in f.outputs:
-                    y().grad = None # y is weakref
-
-    def reshape(self, *shape):
-        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
-            shape = shape[0]
-        return dezero.functions.reshape(self, shape)
-    
-    
-    def transpose(self):
-        return dezero.functions.transpose(self)
-    
-    @property
-    def T(self):
-        return dezero.functions.transpose(self)
     
     @property
     def shape(self):
@@ -123,15 +70,92 @@ class Variable:
         p = str(self.data).replace('\n', '\n' + ' ' * 9)
         return 'variable(' + p + ')'
 
-    def __mul__(self, other):
-        return mul(self, other)
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
     
-    def __add__(self, other):
-        return add(self, other)
+    def unchain(self):
+        self.creator = None    
+    
+    def cleargrad(self):
+        self.grad = None 
+        
+    def backward(self, retain_grad=False, create_graph=False):
+        if self.grad is None:
+            xp = dezero.cuda.get_array_module(self.data)
+            self.grad = Variable(xp.ones_like(self.data))
 
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]  # output is weakref
+
+            with using_config('enable_backprop', create_graph):
+                gxs = f.backward(*gys)
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
+
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else:
+                        x.grad = x.grad + gx
+
+                    if x.creator is not None:
+                        add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None  # y is weakref
+    
+    
+    def unchain_backward(self):
+        if self.creator is not None:
+            funcs = [self.creator]
+            while funcs:
+                f = funcs.pop()
+                for x in f.inputs:
+                    if x.creator is not None:
+                        funcs.append(x.creator)
+                        x.unchain()
+
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+
+    def transpose(self, *axes):
+        if len(axes) == 0:
+            axes = None
+        elif len(axes) == 1:
+            if isinstance(axes[0], (tuple, list)) or axes[0] is None:
+                axes = axes[0]
+        return dezero.functions.transpose(self, axes)    
+    
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
+    
     def sum(self, axis=None, keepdims=False):
         return dezero.functions.sum(self, axis, keepdims)
+    
+    def to_cpu(self):
+        if self.data is not None:
+            self.data = dezero.cuda.as_numpy(self.data)
 
+    def to_gpu(self):
+        if self.data is not None:
+            self.data = dezero.cuda.as_cupy(self.data)    
+    
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
